1341b49ce15cf19cb808076a48002d65a1da26f1
Mwagiru <joymwagiru@gmail.com>
2022-05-17 06:41:18 +0300

    Completed Assignment

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,8 +1,24 @@
 import React from "react";
 import { actors } from "../data";
 
+
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+const actorDetails= actors.map((actor)=>(
+  <div>
+    <h2>{actor.name}</h2>
+    <ul>
+      {actor.movies.map((movie)=>(
+        <li>
+          {movie}
+        </li>
+      ))}
+    </ul>
+  </div>
+))
+  return <div>
+    <h1>Actors Page</h1>
+    {actorDetails}
+   </div>
 }
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,23 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  const movieDetails=movies.map((movie)=>(
+    <div key={movie.title}>
+      <h2>{movie.title}</h2>
+      <h3>{movie.time}</h3>
+      <ul>
+        {movie.genres.map((genre)=>(
+          <li>
+            {genre}
+          </li>
+        ))}
+      </ul>
+    </div>
+  ))
+  return <div>
+    <h1>Movie Page</h1>
+   {movieDetails}
+  </div>;
 }
 
 export default Movies;
